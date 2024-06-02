c819835f830eff831e03f910623d225f8079376d
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-02 14:07:19 +0800

    add top user view, route

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,12 @@ router.post(
 )
 router.get('/logout', userController.logout)
 
-router.get('/restaurants/feeds', authenticated, restController.getFeeds) // 新增這一行
+router.get('/users/top', authenticated, userController.getTopUsers)
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
@@ -37,10 +42,6 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
-
-router.get('/users/:id/edit', authenticated, userController.editUser)
-router.get('/users/:id', authenticated, userController.getUser)
-router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
