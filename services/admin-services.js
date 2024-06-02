7d6b5e8ee0d6a815b4bf7db713c3b0ad13750089
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-02 20:11:25 +0800

    add admin delete restaurant

--- services/admin-services.js
+++ services/admin-services.js
@@ -5,6 +5,19 @@ const adminServices = {
     Restaurant.findAll({ raw: true, nest: true, include: [Category] })
       .then(restaurants => cb(null, { restaurants }))
       .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) {
+          const err = new Error("Restaurant didn't exist!")
+          err.status = 404
+          throw err
+        }
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
   }
 }
 module.exports = adminServices
